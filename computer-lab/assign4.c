3da4d7fd598ce6c660e1e0f703c1b7407d6bb147
Shirmyman1 <shirsha409@gmail.com>
2025-08-26 09:38:49 +0530

    Update assign4.c

--- computer-lab/assign4.c
+++ computer-lab/assign4.c
@@ -7,15 +7,9 @@ int main(){
     printf("enter a number: ");
     scanf("%d",&x);
 
-    printf("%d\n",x*1);
-    printf("%d\n",x*2);
-    printf("%d\n",x*3);
-    printf("%d\n",x*4);
-    printf("%d\n",x*5);
-    printf("%d\n",x*6);
-    printf("%d\n",x*7);
-    printf("%d\n",x*8);
-    printf("%d\n",x*9);
-    printf("%d\n",x*10);
-    return 0;
+    for(int i = 1 ; i <= 10 ; i++)
+    {
+        printf("%d X %d : %d\n",x,i,x*i);
+    }
+        return 0;
 }
