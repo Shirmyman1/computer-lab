0a91c6f79f6db28081110e47330e929b7e5265a9
Shirmyman1 <shirsha409@gmail.com>
2025-09-02 15:52:19 +0530

    Add files via upload

---
+++ computer-lab/day 4/as20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    float x;
+    printf("enter a no: ");
+    scanf("%f",&x);
+    if(x>=0)
+    printf("whole");
+    else
+    printf("not whole");
+    
+    return 0;
+}
